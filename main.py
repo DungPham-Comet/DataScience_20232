89da9526ef218e9037bc079c89d8b38bbac7fff0
dungpt24082002 <dungpt24082002@gmail.com>
2024-05-18 10:30:01 +0700

    Crawl Streamlit

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import streamlit as st
 import os
 import json
 
-current_dir = os.path.dirname(__file__)
-file_path = os.path.join(current_dir, '', 'laptopCrawler', 'laptopCrawler', 'DataCrawl', 'test.json')
 
-absolute_path = os.path.abspath(file_path)
-print(f"Đường dẫn tuyệt đối: {absolute_path}")
+# current_dir = os.path.dirname(__file__)
+# file_path = os.path.join(current_dir, '', 'laptopCrawler', 'laptopCrawler', 'DataCrawl', 'test.json')
+#
+# absolute_path = os.path.abspath(file_path)
+# print(f"Đường dẫn tuyệt đối: {absolute_path}")
+#;
+#
+# with open(absolute_path, 'r', encoding='utf-8') as file:
+#     data = json.load(file)
 
-
-with open(absolute_path, 'r', encoding='utf-8') as file:
-    data = json.load(file)
+if st.button('Crawl data'):
+    os.system('cd laptopCrawler && python aps_crawl.py')
